0491ef160aa7c37a2cec5b4010fe256cb9e238cd
Ruling Fong <Ruling Fong>
2016-10-07 13:46:05 +0800

    hi

---
+++ client/static/js/components/Navi.js
@@ -0,0 +1,14 @@
+'use strict';
+var React = require('react');
+
+export default class Navigator extends React.Component {
+    render() {
+        return (
+            <<nav>
+                <a>首页</a>
+                <a></a>
+            </nav>
+        )
+    }
+}
+
